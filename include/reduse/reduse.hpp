29f758fa63679eb63cd1d7977fe88d7498435a16
Stumblef00l <kunalbonucci@gmail.com>
2021-06-21 02:35:28 +0530

    Add test for reduse(). Some issues still remaining

--- include/reduse/reduse.hpp
+++ include/reduse/reduse.hpp
@@ -1,15 +1,35 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
 #include <functional>
 #include <utility>
+#include <reduse/mapper.hpp>
+#include <reduse/reducer.hpp>
 
 namespace reduse {
     template<typename map_key, typename map_value, typename reduce_value>
     void reduse(
-        const int num_mappers,
-        const int num_redusers,
         const std::string input_filename,
+        const std::string output_filename,
         const std::function<std::pair<map_key, map_value>(const std::string&)> &MAP, 
-        const std::function<reduce_value(std::pair<map_key, map_value>)> &REDUCE,
+        const std::function<reduce_value(map_key, std::vector<map_value>&)> &REDUCE,
+        const int num_mappers = DEFAULT_NUM_MAPPERS,
+        const int num_reducers = DEFAULT_NUM_REDUCERS,
         const bool verbose = false
-    );
+    ) {
+        std::string map_output_filename = input_filename + "_map_output.txt";
+        try {
+            Mapper<map_key, map_value> mapper(input_filename, map_output_filename, MAP, num_mappers, verbose);
+            mapper.run();
+            Reducer<map_key, map_value, reduce_value> reducer(map_output_filename, output_filename, REDUCE, num_reducers, verbose);
+            reducer.run();
+            if(remove(map_output_filename.c_str()) != 0)
+                throw std::runtime_error("Unable to delete " + map_output_filename);
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            remove(map_output_filename.c_str());
+            std::terminate();
+        }
+    }
 }
---
+++ tests/TestReduse.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <exception>
+#include <unordered_map>
+#include <gtest/gtest.h>
+#include <config.h>
+#include <reduse/reduse.hpp>
+
+// Map method
+std::pair<int, int> REDUSE_MAP(const std::string& s) {
+    auto a = (s[0] - '0');
+    auto b = s.substr(1, s.length() - 1);
+    return {a, stoi(b)};
+}
+
+// Reduce method
+int REDUSE_REDUCE(int key, std::vector<int>& values) {
+    auto sum = 0;
+    for(auto &it: values)
+        sum += it;
+    return sum;
+}
+
+TEST(TestReduse, TestRun) {
+    // Define the input file
+    std::string input_filename = TEST_SOURCE_DIR;
+    input_filename += "/testreduse_input.txt";
+    std::string output_filename = TEST_SOURCE_DIR;
+    output_filename += "/testreduse_output.txt";
+
+    // Create the mapper and run it
+    try {
+        reduse::reduse<int, int, int> (input_filename, output_filename, REDUSE_MAP, REDUSE_REDUCE, 3, 3);
+    } catch (const std::exception &e) {
+        std::cout << e.what();
+        std::terminate();
+    }
+
+    // Open the output file produced by the reducer
+    std::fstream output_file;
+    output_file.open(output_filename, std::ios::in);
+
+    auto ct = 0; // Number of items in the file
+    std::unordered_map<int, int> output_file_map; // Frequency map for each item in the file
+    int item; // Buffer for the current item in the file
+
+    // Iterate over the output file and count number of items and each of their frequencies
+    while(output_file >> item) {
+        output_file_map[item]++;
+        ct++;
+    }
+
+    // Assertions
+    ASSERT_EQ(ct, 3);
+    ASSERT_EQ((int)(output_file_map.size()), 2);
+    ASSERT_EQ(output_file_map[323], 2);
+    ASSERT_EQ(output_file_map[15], 1);
+}
